73ea7a456be47ab327d313ec5e4c015d97dd9e0d
infiooo <113079767@qq.com>
2016-12-23 18:33:55 +0800

    123

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@
  * @Author: 张驰阳
  * @Date:   2016-12-22 15:11:46
  * @Last Modified by:   张驰阳
- * @Last Modified time: 2016-12-22 17:45:46
+ * @Last Modified time: 2016-12-23 17:34:54
  */
 
 'use strict';
@@ -10,19 +10,26 @@
 var express = require("express");
 var app = express();
 var fortune = require("./lib/fortune.js");
+var weather = require("./lib/weather.js");
 //设置handlebars试图引擎
-var handlebars = require("express3-handlebars").create({ defaultLayout: "main" }); //默认布局
+var handlebars = require("express3-handlebars").create({ defaultLayout: "main", extname: '.hbs' }); //默认布局 修改后缀
 
-
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
+app.engine('.hbs', handlebars.engine); //加载handlebars引擎
+app.set("view engine", ".hbs");
 
 //启动服务器前通过设置环境变量覆盖端口
 app.set("port", process.env.PORT || 3000);
 //创建公共目录
 app.use(express.static(__dirname + "/public"));
-
-//测试中间件
+//创建局部文件中间件
+app.use(function(req, res, next) {
+        if (!res.locals.partials) {
+            res.locals.partials = {};
+        }
+        res.locals.partials.weather = weather.getWeatherData();
+        next();
+    })
+    //测试中间件
 app.use(function(req, res, next) {
     res.locals.showTests = app.get("env") !== "production" &&
         req.query.test === "1";
@@ -35,17 +42,40 @@ app.get("/", function(req, res) {
 });
 app.get("/about", function(req, res) {
     res.render("about", {
+        // layout:null,
         fortune: fortune.getFortune(),
         //配置about页面测试
         pageTestScript: '/qa/tests-about.js'
     });
 });
-app.get("/tours/hood-river",function(req,res){
-	res.render("tours/hood-river");
+app.get("/tours/hood-river", function(req, res) {
+    res.render("tours/hood-river");
 });
-app.get("/tours/request-group-rate",function(req,res){
-	res.render("tours/request-group-rate");
+app.get("/tours/request-group-rate", function(req, res) {
+    res.render("tours/request-group-rate");
 });
+
+
+//test
+//查看浏览器发送的信息
+// app.get("/headers",function(req,res){
+//     res.type("text/plain");
+//     var s="";
+//     for(var name in req.headers ) s+=name+":"+req.headers[name]+"\n";
+//         res.send(s);
+// });
+// app.get("/greeting",function(req,res){
+//     res.render("about",{
+//         message:"welcome",
+//         style:req.query.style,
+//         userid:req.cookie.userid,
+//         username:req.session.username
+//     })
+// })
+
+
+
+
 
 //定制404页面
 app.use(function(req, res) {
